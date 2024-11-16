a3b9bb5ce585ff6e32f57f91bb249ee4fa1c44ec
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-16 17:56:17 +0600

    189. Rotate Array

---
+++ Top-Interview-150/rotateArray.cpp
@@ -0,0 +1,29 @@
+//189. Rotate Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+
+        reverse(nums.begin(), nums.begin() + k);
+
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    solution.rotate(nums, k);
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
