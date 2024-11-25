e299da87231fd53045e3573aa54c4a636a0fc79a
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-25 23:59:37 +0600

    Update integerToRoman.cpp

--- Top-Interview-150/integerToRoman.cpp
+++ Top-Interview-150/integerToRoman.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 string intToRoman(int num)
 {
-    vector<pair<string, int>> symList = {
-        {"I", 1}, {"IV", 4}, {"V", 5}, {"IX", 9}, {"X", 10}, {"XL", 40}, {"L", 50}, {"XC", 90}, {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900}, {"M", 1000}};
+    vector<pair<string, int>> symList = {{"I", 1}, {"IV", 4}, {"V", 5}, {"IX", 9}, {"X", 10}, {"XL", 40}, {"L", 50}, {"XC", 90}, {"C", 100}, {"CD", 400}, {"D", 500}, {"CM", 900}, {"M", 1000}};
 
     string result = "";
 
